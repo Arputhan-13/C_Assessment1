a3d0ed898a830b4ef91a01c2d55395d1a45880c7
Arputhan-13 <karputhan@gmail.com>
2026-06-02 22:20:06 +0530

    Create C_Problem_22.c

---
+++ C_Problem_22.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the 3digit number :");
+    scanf("%d",&a);
+    b=a-5*((a/10)%2);
+    printf(" The result : %d",b);
+    return 0;
+}    
+    
